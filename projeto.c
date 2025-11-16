b58fb88792a740a6798841b2cafed7f00a40880f
Catarina Silva e Meirelles <catlls622@gmail.com>
2025-11-16 13:29:39 +0000

    resolução de mais problemas no código e remoção de comentários como pedido no trabalho

--- projeto.c
+++ projeto.c
@@ -1,23 +1,15 @@
 /*Lívia Calado de Carvalho Dias 10737709
   Catarina Silva E Meirelles    10239324
-  Beatriz de Assis Siqueira     10741570*/
+  Beatriz de Assis Siqueira     10741570
+  link do video: 
+  
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Assinatura da codificar_imagem-vamos precisar dela 
 void codificar_imagem(int altura, int largura, int matriz[altura][largura], char resultado[]);
-
-void printImg(int altura, int largura, int matriz[altura][largura]){
-   printf("\n----");
-    for(int i = 0; i < altura; i++){
-        printf("\n");
-        for(int j = 0; j < largura; j++){ 
-            printf(" %d", matriz[i][j]);
-        }
-   }
-}
 
 void copiarMatriz(  int tamanhoCopiaX,
                     int tamanhoCopiaY, 
@@ -35,22 +27,19 @@ void copiarMatriz(  int tamanhoCopiaX,
 }
 
 void cortar(int altura, int largura, int matriz[altura][largura], char resultado[]){
-    int X2 = altura/2;//divisão inteira
+    int X2 = altura/2;
     int X1 = altura-X2;
     int Y2 = largura/2;
     int Y1 = largura-Y2;
-    printf("\n<%d><%d><%d><%d>\n", X1, X2, Y1, Y2);
     if(X1 != 0){
         if(Y1 != 0){
             int md1[X1][Y1];
             copiarMatriz(X1,Y1, md1, altura, largura, matriz, 0,X1,0,Y1);
-            printImg(X1,Y1,md1);
             codificar_imagem(X1,Y1, md1, resultado);
         }
         if(Y2 != 0){
             int md2[X1][Y2];
             copiarMatriz(X1,Y2, md2, altura,largura, matriz, 0,X1,Y1,largura);
-            printImg(X1,Y2,md2);
             codificar_imagem(X1,Y2, md2, resultado);
         }
     }
@@ -58,23 +47,19 @@ void cortar(int altura, int largura, int matriz[altura][largura], char resultado
         if(Y1 != 0){
             int md3[X2][Y1];
             copiarMatriz(X2,Y1, md3, altura,largura, matriz, X1,altura,0,Y1);
-            printImg(X2,Y1,md3);
             codificar_imagem(X2,Y1, md3, resultado);
         }
         if(Y2 != 0){
             int md4[X2][Y2];
             copiarMatriz(X2,Y2, md4, altura,largura, matriz, X1,altura,Y1,largura);
-            printImg(X2,Y2,md4);
             codificar_imagem(X2,Y2, md4, resultado);
         }
     }
 }
 
-void codificar_imagem(int altura, int largura, int matriz[altura][largura], char resultado[]){//cat
-    //analisar se a imagem inteira está completa, se não, cortá-la
+void codificar_imagem(int altura, int largura, int matriz[altura][largura], char resultado[]){
     char preenchimento[2];
     int referencia = matriz[altura-1][largura-1];
-    //printf("\nreferencia =<%d>\n", referencia);
 
     if (referencia == 0){
             preenchimento[0] = 'B';
@@ -84,40 +69,34 @@ void codificar_imagem(int altura, int largura, int matriz[altura][largura], char
 
     if (altura <= 1 && largura <= 1){
         strcat(resultado, preenchimento);
-        //printf("\nAltura atual: %d, Larg: %d, item[]: <%d>\n", altura, largura, matriz[altura-1][largura-1]);
         return;
     }
 
     for(int i=0; i<altura; i++){
         for(int j=0; j<largura; j++){
-            //printf("\nmatriz[%d][%d]= <%d>\n", i,j, matriz[i][j]);
             if (matriz[i][j] != referencia){
                 preenchimento[0] = 'X';
                 strcat(resultado, preenchimento);
-                printf("\ncorte\n");
                 cortar(altura, largura, matriz, resultado);
                 return;
             }
         }
     } 
-
     strcat(resultado, preenchimento);
-    printf("\n percorreu loop ->%s", resultado);
-
 }
 
-void modo_manual(char resultado[]){//Livia
+void modo_manual(char resultado[]){
     int largura, altura;
     printf("Modo Manual Ativado;\n");
     do{
         printf("Informe a largura da imagem: ");
-        scanf("%d", &largura);
+        scanf(" %d", &largura);
         printf("Informe a altura da imagem: ");
-        scanf("%d", &altura);
+        scanf(" %d", &altura);
         if (largura <= 0 || largura > 1024 || altura <= 0 || altura > 768){
             printf("Dimensões inválidas. A largura deve ser entre 1 e 1024, e a altura entre 1 e 768;\n->Tente novamente!\n");
         }
-    }while (largura <= 0 || largura > 1024 || altura <= 0 || altura > 768);
+    }while(largura <= 0 || largura > 1024 || altura <= 0 || altura > 768);
     
     
     int matriz[altura][largura];
@@ -136,28 +115,41 @@ void modo_manual(char resultado[]){//Livia
     return;
 }
 
-int modo_arquivo(char *file, char resultado[]){//bea
-    int largura, altura;
+int modo_arquivo(char *filename, char resultado[]){
+    int largura = 0;
+    int altura = 0;
     char tipo[3];
     char coment[1000];
     FILE *file;     
 
-    file = fopen(file[1], "r");
-
+    file = fopen(filename, "r");
+    
     if(file == NULL){
-        printf("Erro ao abrir o arquivo. Arquivo vazio.");
+        printf("\nErro ao abrir o arquivo %s. Arquivo vazio.", filename);
         return 1;
     }
-  
+
     fgets(tipo, sizeof(tipo), file);
-    while(fgets(coment, sizeof(coment), file)){
-      if(coment[0] == '#') continue;
+
+    while (fgets(coment, sizeof(coment), file)) {
+        if (coment[0] == '#') {
+            continue;
+        } 
+        if (sscanf(coment, "%d %d", &altura, &largura) == 2) {
+            break; 
+        }
     }
-    fscanf(file, "%d %d", &altura, &largura);
+
+    if (largura == 0 || altura == 0) {
+        printf("\nErro: Formato inválido ou dimensões não encontradas.\n");
+        fclose(file);
+        return 1;
+    }
+
     int matriz[altura][largura];
 
-    for(int i = 0; i <= altura; i++){
-        for(int j = 0; j <= largura; j++){
+    for(int i = 0; i < altura; i++){
+        for(int j = 0; j < largura; j++){
             fscanf(file, "%d", &matriz[i][j]);
         }
     }
@@ -174,12 +166,11 @@ void exibir_ajuda(){
     printf("Argumentos:\n");
     printf(" -?, --help : apresenta essa orientação na tela;\n");
     printf("-m, --manual : ativa o modo de entrada manual, em que o usuário fornece todos os dados da imagem infomando-os através do teclado;\n");
-    printf("-f, --file : considera a imagem respresentada no arquivo PBM (Portable Bitmap)\n");
+    printf("-f, --file : considera a imagem respresentada no arquivo PBM (Portable Bitmap)\n->para colocar a imagem desejada, coloque-a na pasta 'arquivosImagensPBM'\ne digite -f arquivosImagensPBM/nomeDoArquivo.pbm no terminal do programa.");
 }
 
 int main(int argc, char *argv[]){
     if(argc == 1 || strcmp(argv[1], "-?") == 0 || strcmp(argv[1], "--help") == 0){
-        //mostra a função que exibe a ajuda
         exibir_ajuda();
         return 0;
     }
@@ -187,12 +178,10 @@ int main(int argc, char *argv[]){
     char resultado[500];
     
     if (strcmp(argv[1], "-m") == 0 || strcmp(argv[1], "--manual") == 0){
-        //ativa o modo manual
         modo_manual(resultado);
     }
     else if(strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "--file") == 0){
-        //ativa o modo do arquivo
-        int retARQ = modo_arquivo(argv[6], resultado);
+        int retARQ = modo_arquivo(argv[2], resultado);
         if (retARQ != 0){
             return 1;
         }
